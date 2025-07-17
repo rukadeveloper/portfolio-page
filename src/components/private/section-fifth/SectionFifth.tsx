179ddca6e2a908a958ddc00f4c0ae4b3a9eba43d
rukadeveloper <kansaki135@gmail.com>
2025-07-17 08:45:54 -0700

    project modal created.

--- src/components/private/section-fifth/SectionFifth.tsx
+++ src/components/private/section-fifth/SectionFifth.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
 
 import styled from "styled-components";
 
@@ -6,6 +7,9 @@ import NestedTitle from "../../shared/titler/NestedTitle";
 import PageInner from "../../shared/page-inner/PageInner";
 
 import useScrollData from "../../../store/useScrollData";
+import { projectsMocks } from "../../../static/mocks/projectsMocks";
+
+import { IoCloseOutline } from "react-icons/io5";
 
 const SectionFive = styled.section`
   background-color: #000;
@@ -86,10 +90,153 @@ const ProjectsImg = styled.div`
   }
 `;
 
+const ProjectModal = styled.div`
+  position: fixed;
+  width: 850px;
+  height: 800px;
+  background-color: #fff;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  z-index: 500;
+  border-radius: 10px;
+  overflow: hidden;
+  > button {
+    background-color: transparent;
+    border: none;
+    position: absolute;
+    top: 30px;
+    right: 30px;
+    z-index: 500;
+    cursor: pointer;
+  }
+`;
+
+const ProjectHeader = styled.div`
+  font-family: "SUIT", sans-serif !important;
+  background-color: #4ea9ff;
+  h2 {
+    color: black;
+    font-size: 1.1rem;
+    text-align: center;
+    font-weight: 800;
+    padding-top: 2.2rem;
+  }
+  h3 {
+    text-align: center;
+    padding-top: 0.9rem;
+  }
+  ul {
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    margin-top: 20px;
+    padding-bottom: 20px;
+    li {
+      color: #fff;
+      background-color: #000;
+      padding: 0.3rem 0.5rem;
+      font-size: 0.885rem;
+      border-radius: 6px;
+    }
+  }
+  span {
+    display: block;
+    text-align: center;
+    padding-bottom: 2.6rem;
+  }
+`;
+
+const ProjectBody = styled.div`
+  font-family: "SUIT", sans-serif !important;
+  max-width: 80%;
+  margin: 0 auto;
+  h1 {
+    padding-top: 1.1rem;
+    padding-bottom: 1.8rem;
+    text-align: center;
+    line-height: 1.6;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.4);
+    margin-bottom: 20px;
+  }
+  dl {
+    margin-bottom: 26px;
+    dd {
+      padding-left: 20px;
+      position: relative;
+      &::before {
+        content: "";
+        position: absolute;
+        width: 6px;
+        height: 6px;
+        background-color: black;
+        border-radius: 50%;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        margin: auto 0;
+      }
+    }
+    dt {
+      padding-left: 20px;
+      font-size: 0.9rem;
+      padding-top: 10px;
+      font-weight: 200;
+    }
+    &.languages {
+      display: flex;
+      flex-wrap: wrap;
+      dd {
+        width: 100%;
+        margin-bottom: 16px;
+      }
+      dt {
+        color: #fff;
+        background-color: #4ea9ff;
+        padding: 0.5rem;
+        margin-right: 10px;
+      }
+    }
+    &.contribution {
+      dd,
+      dt {
+        width: 100%;
+      }
+      dt {
+        background-color: #d9d9d9;
+        padding: 0.5rem;
+        margin-top: 16px;
+        font-weight: 700;
+      }
+    }
+  }
+`;
+
 const SectionFifth = () => {
   const section4Ref = useRef<HTMLElement | null>(null);
 
+  const [isModal, setIsModal] = useState({
+    is: false,
+    idx: 0,
+  });
+
   const { changeFour } = useScrollData();
+
+  const goModal = (e: React.MouseEvent<HTMLAnchorElement>, ixx: number) => {
+    e.preventDefault();
+    setIsModal({
+      is: true,
+      idx: ixx,
+    });
+    document.body.classList.add("dimmed");
+  };
+
+  const closeModal = () => {
+    setIsModal({ ...isModal, is: false });
+    document.body.classList.remove("dimmed");
+  };
 
   useEffect(() => {
     changeFour(section4Ref.current!.getBoundingClientRect().top, section4Ref);
@@ -105,45 +252,78 @@ const SectionFifth = () => {
       <Projects>
         <PageInner isHeader={false}>
           <ProjectsWrapper>
-            <li>
-              <a href="https://rukadeveloper.github.io">
-                <ProjectsImg>
-                  <img
-                    src="/images/projects/thumbnail_01.png"
-                    alt="thumbnail_1"
-                  />
-                </ProjectsImg>
-                <h3>Baseball Comics v2</h3>
-                <span>베이스볼 코믹스 v2</span>
-              </a>
-            </li>
-            <li>
-              <a href="http://partner-finance.co.kr/cple/">
-                <ProjectsImg>
-                  <img
-                    src="/images/projects/thumbnail_02.png"
-                    alt="thumbnail_2"
-                  />
-                </ProjectsImg>
-                <h3>안심 근로자 대출</h3>
-                <span>근로자들을 위한 대출 사이트!</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://kor-bankpartner.co.kr/auto2369360033202411/">
-                <ProjectsImg>
-                  <img
-                    src="/images/projects/thumbnail_03.png"
-                    alt="thumbnail_3"
-                  />
-                </ProjectsImg>
-                <h3>자동차 담보 대출</h3>
-                <span>자동차 담보로 대출자들을 위한 사이트!</span>
-              </a>
-            </li>
+            {projectsMocks.map((pm, ix) => (
+              <li>
+                <a
+                  href={pm.link}
+                  onClick={(e) => {
+                    goModal(e, ix);
+                  }}
+                >
+                  <ProjectsImg>
+                    <img src={pm.imageUrl} alt="thumbnail_1" />
+                  </ProjectsImg>
+                  <h3>{pm.subject}</h3>
+                  <span>{pm.second_subject}</span>
+                </a>
+              </li>
+            ))}
           </ProjectsWrapper>
         </PageInner>
       </Projects>
+      {isModal.is &&
+        ReactDOM.createPortal(
+          <ProjectModal>
+            <ProjectHeader>
+              <h2>프로젝트 소개</h2>
+              <h3>{projectsMocks[isModal.idx].second_subject}</h3>
+              <ul>
+                {projectsMocks[isModal.idx].modals.tag.map((t) => (
+                  <li>{t}</li>
+                ))}
+              </ul>
+              <span>
+                {projectsMocks[isModal.idx].modals.startPeriod} ~{" "}
+                {projectsMocks[isModal.idx].modals.endPeriod}
+              </span>
+            </ProjectHeader>
+            <ProjectBody>
+              <h1>
+                {projectsMocks[isModal.idx].modals.details.split("|")[0]}
+                <br />
+                {projectsMocks[isModal.idx].modals.details.split("|")[1]}
+              </h1>
+              <dl className="functions">
+                <dd>주요 기능</dd>
+                {projectsMocks[isModal.idx].modals.functions.map((f) => (
+                  <dt>{f}</dt>
+                ))}
+              </dl>
+              <dl className="languages">
+                <dd>사용기술 및 언어</dd>
+                {projectsMocks[isModal.idx].modals.usedLanguage.map((l) => (
+                  <dt>{l}</dt>
+                ))}
+              </dl>
+              <dl className="contribution">
+                <dd>작업 기여도</dd>
+                {projectsMocks[isModal.idx].modals.contribution.map((d) => (
+                  <dt>{d}</dt>
+                ))}
+              </dl>
+              <dl className="trouble-shooting">
+                <dd>트러블 슈팅</dd>
+                {projectsMocks[isModal.idx].modals.troubleShooting.map((t) => (
+                  <dt>{t}</dt>
+                ))}
+              </dl>
+            </ProjectBody>
+            <button onClick={closeModal}>
+              <IoCloseOutline size={26} color={"#fff"} />
+            </button>
+          </ProjectModal>,
+          document.getElementById("root")!
+        )}
     </SectionFive>
   );
 };
---
+++ src/static/mocks/projectsMocks.ts
@@ -0,0 +1,95 @@
+export const projectsMocks = [
+  {
+    link: "https://rukadeveloper.github.io",
+    imageUrl: "/images/projects/thumbnail_01.png",
+    subject: "Baseball Comics v2",
+    second_subject: "베이스볼 코믹스 v2",
+    modals: {
+      tag: ["사이드", "React", "TS"],
+      startPeriod: "2025.06.01",
+      endPeriod: "2025.07.01",
+      details:
+        "Baseball 직관 프로그램을 직접 구현한 React 기반 애플리케이션입니다.|현재 진행중이며, 메인 페이지만 구현된 상태입니다.",
+      functions: [
+        "로그인 및 회원가입 로그아웃 기능 구현",
+        "스크롤 슬라이더 기능 구현",
+      ],
+      usedLanguage: [
+        "React",
+        "zustand",
+        "typescript",
+        "styled-component",
+        "Spring Boot",
+      ],
+      contribution: [
+        "Spring Boot을 이용한 CRUD 기능 구현 통한 로그인 / 회원가입 기능",
+        "슬라이더 기능 구현 시 Custom Hooks 통해 유지 보수 관리",
+      ],
+      troubleShooting: [
+        "슬라이더에서 overflow-hidden을 사용했으므로, 직접 sticky 효과를 custom-hooks로 해결",
+      ],
+    },
+  },
+  {
+    link: "https://one-bestfinance.co.kr/DG9557343792/",
+    imageUrl: "/images/projects/thumbnail_02.png",
+    subject: "최저금리 안심 서비스",
+    second_subject: "최저 금리 안심 서비스",
+    modals: {
+      tag: ["사이드", "React", "TS"],
+      startPeriod: "2025.06.01",
+      endPeriod: "2025.07.01",
+      details:
+        "Baseball 직관 프로그램을 직접 구현한 React 기반 애플리케이션입니다.|현재 진행중이며, 메인 페이지만 구현된 상태입니다.",
+      functions: [
+        "로그인 및 회원가입 로그아웃 기능 구현",
+        "스크롤 슬라이더 기능 구현",
+      ],
+      usedLanguage: [
+        "React",
+        "zustand",
+        "typescript",
+        "styled-component",
+        "Spring Boot",
+      ],
+      contribution: [
+        "Spring Boot을 이용한 CRUD 기능 구현 통한 로그인 / 회원가입 기능",
+        "슬라이더 기능 구현 시 Custom Hooks 통해 유지 보수 관리",
+      ],
+      troubleShooting: [
+        "슬라이더에서 overflow-hidden을 사용했으므로, 직접 sticky 효과를 custom-hooks로 해결",
+      ],
+    },
+  },
+  {
+    link: "https://kor-bankpartner.co.kr/auto2369360033202411/",
+    imageUrl: "/images/projects/thumbnail_03.png",
+    subject: "자동차 안심 대출",
+    second_subject: "자동차 안심 대출",
+    modals: {
+      tag: ["사이드", "React", "TS"],
+      startPeriod: "2025.06.01",
+      endPeriod: "2025.07.01",
+      details:
+        "Baseball 직관 프로그램을 직접 구현한 React 기반 애플리케이션입니다.|현재 진행중이며, 메인 페이지만 구현된 상태입니다.",
+      functions: [
+        "로그인 및 회원가입 로그아웃 기능 구현",
+        "스크롤 슬라이더 기능 구현",
+      ],
+      usedLanguage: [
+        "React",
+        "zustand",
+        "typescript",
+        "styled-component",
+        "Spring Boot",
+      ],
+      contribution: [
+        "Spring Boot을 이용한 CRUD 기능 구현 통한 로그인 / 회원가입 기능",
+        "슬라이더 기능 구현 시 Custom Hooks 통해 유지 보수 관리",
+      ],
+      troubleShooting: [
+        "슬라이더에서 overflow-hidden을 사용했으므로, 직접 sticky 효과를 custom-hooks로 해결",
+      ],
+    },
+  },
+];
